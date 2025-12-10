c1c5488bd3cf8e5ce9b7a9fbb0ae8d39788f4980
Md Noman Miazi <96537881+mdnomanmiazi@users.noreply.github.com>
2025-12-10 23:38:03 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -22,15 +22,17 @@ export default function Home() {
   const models = [
     "gpt-5.1",
     "gpt-5",
-    "gpt-5.1-codex",
-    "gpt-5.1-codex-max",
-    "gpt-5-codex",
-    "gpt-5.1-codex-mini",
+    "gpt-5-mini",
+    "gpt-5-nano",
+    "gpt-5-pro",
     "gpt-4.1",
     "gpt-4.1-mini",
     "gpt-4.1-nano",
+    "gpt-4-turbo",
+    "gpt-4",
+    "gpt-4o",
+    "gpt-4o-mini",
     "gpt-3.5-turbo",
-    "codex-mini-latest"
   ];
 
   const router = useRouter();
